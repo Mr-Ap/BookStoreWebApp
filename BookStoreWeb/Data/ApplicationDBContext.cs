2c087a84d71a1c5bf05825f5b3931824445f8501
Avadhut Patil <Avadhut.Patil@mindbodyonline.com>
2022-07-25 14:43:02 +0530

    created a database and connceted to ssms

--- BookStoreWeb/Data/ApplicationDBContext.cs
+++ BookStoreWeb/Data/ApplicationDBContext.cs
@@ -1,8 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BookStoreWeb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookStoreWeb.Data
 {
     public class ApplicationDBContext:DbContext
     {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
