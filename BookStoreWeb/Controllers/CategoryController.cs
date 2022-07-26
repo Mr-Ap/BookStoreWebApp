1fc0b2478e6d9be3a6bea02fe4ad71db2db3025e
Avadhut Patil <Avadhut.Patil@mindbodyonline.com>
2022-07-27 00:12:21 +0530

    performed crud operations

---
+++ BookStoreWeb/Controllers/CategoryController.cs
@@ -0,0 +1,92 @@
+﻿using BookStoreWeb.Data;
+using BookStoreWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDBContext _dbc;
+
+        public CategoryController(ApplicationDBContext dbc)
+        {
+            _dbc = dbc;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categoryList = _dbc.Categories.ToList();
+            return View(categoryList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbc.Categories.Add(obj);
+                _dbc.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+
+        //Get
+        public IActionResult Edit(int id)
+        {
+            if (id is 0)
+                return NotFound();
+            //Category categorySingle = _dbc.Categories.SingleOrDefault(c => c.Id == id);
+            //Category categoryFirst = _dbc.Categories.FirstOrDefault(c => c.Id == id);
+            Category category = _dbc.Categories.Find(id);
+            if (category == null)
+                return NotFound();
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbc.Categories.Update(obj);
+                _dbc.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+
+        public IActionResult Delete(int id)
+        {
+            if (id is 0)
+                return NotFound();
+            //Category categorySingle = _dbc.Categories.SingleOrDefault(c => c.Id == id);
+            //Category categoryFirst = _dbc.Categories.FirstOrDefault(c => c.Id == id);
+            Category category = _dbc.Categories.Find(id);
+            if (category == null)
+                return NotFound();
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int id)
+        {
+            Category category = _dbc.Categories.Find(id);
+            if (category is null)
+                return NotFound();
+
+            _dbc.Categories.Remove(category);
+            _dbc.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- BookStoreWeb/Models/Category.cs
+++ BookStoreWeb/Models/Category.cs
@@ -1,13 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BookStoreWeb.Models
 {
     public class Category
     {
+        private const int V = 100;
+
         [Key]
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage ="Display Order should be in between 1 and 100!")]
         public int DisplayOrder { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
